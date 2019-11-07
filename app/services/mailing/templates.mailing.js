f4a722a146e1bd9daadc70b3bfdb5ec33711a9a0
WebWalterWrite <webwalterwhrite@gmail.com>
2019-11-07 14:21:28 +0100

    type: data desc: template id de mailjet

---
+++ app/services/mailing/templates.mailing.js
@@ -0,0 +1,14 @@
+const templates = {
+  CONFIRM: {
+    name: 'confirm',
+    id: 1070537
+  },
+  SUBSCRIBE:{
+    name: 'subscribe',
+    id: 1051336
+  }
+};
+
+Object.freeze(templates);
+
+export default templates;
